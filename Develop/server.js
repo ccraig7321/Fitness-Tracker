1d82221f6a66625bc2da46137dcacc2ada450e5a
Chelsey Craig <chelsey.craig7321@gmail.com>
2020-07-29 19:57:15 -0400

    Updated server file to have working homepage

--- Develop/server.js
+++ Develop/server.js
@@ -1,107 +1,3 @@
-// const express = require("express");
-// const logger = require("morgan");
-// const mongoose = require("mongoose");
-
-// const PORT = process.env.PORT || 3001;
-
-// const db = require("./models/workout.js");
-// const { response } = require("express");
-
-// const app = express();
-
-// app.use(logger("dev"));
-
-// app.use(express.urlencoded({ extended: true }));
-// app.use(express.json());
-
-// app.use(express.static("public"));
-
-// mongoose.connect( "mongodb://localhost/workout", { useNewUrlParser: true });
-
-// // mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/populatedb", { useNewUrlParser: true });
-
-// app.get("/", (req, res) => {
-//     // db.create(data)
-//     // .then(function (response){
-//     //     console.log(response);
-//     // })
-//     // .catch(({ message }) => {
-//     //     console.log(message);
-//     //   });
-//     db.find()
-//     .then(function(response){
-//         console.log(response)
-//     })
-//     res.sendFile(__dirname + "/public/index.html")
-//   });
-
-// app.get("/stats", (req, res) => {
-//     db.find()
-//     .then(function(response){
-//         console.log(response)
-//     })
-//     res.sendFile(__dirname + "/public/stats.html")
-// })
-
-// app.get("/exercise", (req, res) => {
-//     db.find()
-//     .then(function(response){
-//         console.log(response)
-//     })
-//     res.sendFile(__dirname + "/public/exercise.html")
-// })
-
-// app.put("/api/workouts/:id", (req, res) => {
-//     console.log(req.body)
-//     db.Workout.create({day: Date.now(), exercises: [req.body]})
-//     .then(dbExample => {
-//       console.log("Success!", dbExample);
-//       res.send(201)
-//     })
-//     .catch(({ message }) => {
-//       console.log("error", message);
-//       res.send(400)
-//     });
-// })
-
-// app.get("/api/workouts", (req, res) => {
-//     db.Workout.find({}).populate
-//     .then(dbResponse => {
-//         console.log("Response", dbResponse);
-//         res.json(dbResponse)
-//     })
-//     .catch(({ message }) => {
-//         console.log("error", message);
-//         res.send(400)
-//       });
-// })
-
-// app.get("/api/workouts/range", (req, res) => {
-//     db.Workout.find()
-//     .then(dbResponse => {
-//         console.log("Response", dbResponse);
-//         res.json(dbResponse)
-//     })
-//     .catch(({ message }) => {
-//         console.log("error", message);
-//         res.send(400)
-//       });
-// })
-
-// // app.get("/exercise", (req, res) => {
-// //     db.find()
-// //     .then(function(response){
-// //         console.log(response)
-// //     })
-// //     res.sendFile("./public/exercise.html")
-// // })
-
-
-// // Start the server
-// app.listen(PORT, () => {
-//     console.log(`App running on port ${PORT}!`);
-//   });
-
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
@@ -122,40 +18,20 @@ app.use(express.static("public"));
 
 mongoose.connect( "mongodb://localhost/workout", { useNewUrlParser: true });
 
-// mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/populatedb", { useNewUrlParser: true });
 
 app.get("/", (req, res) => {
-    // db.create(data)
-    // .then(function (response){
-    //     console.log(response);
-    // })
-    // .catch(({ message }) => {
-    //     console.log(message);
-    //   });
-    // db.find()
-    // .then(function(response){
-    //     console.log(response)
-    // })
+
     res.sendFile(__dirname + "/public/index.html")
   });
 
 app.get("/stats", (req, res) => {
-    // db.find()
-    // .then(function(response){
-    //     console.log(response)
-    // })
+
     res.sendFile(__dirname + "/public/stats.html")
 })
 
 app.get("/exercise", (req, res) => {
-    // db.find()
-    // .then(function(response){
-    //     console.log(response)
-    // })
     res.sendFile(__dirname + "/public/exercise.html")
 })
-
-// mongoose.Types.ObjectId[
 
 app.put("/api/workouts/:id", (req, res) => {
     console.log(req.body)
@@ -169,58 +45,20 @@ app.put("/api/workouts/:id", (req, res) => {
     });
 })
 
-// .populate("exercises")
-
-// app.get("/api/workouts", (req, res) => {
-//     db.Workout.find()
-//     .then(dbResponse => {
-
-//     // OUTER LOOP: loop though dbResponse to loop through all the workout (looping through workouts/days of the week)
-//         let newWorkoutArray = [];
-
-//         for(let i =0; i < dbResponse; i++) {
-//         let newWorkoutObject;
-//         let totalDuration = 0; {
-//             for(let j=0; j < exercises.length; j++) {
-//                 totalDuration += exercises[j].duration;
-//             }
-        // newWorkoutObject.totalDuration = totalDuration,
-        // newWorkoutObject.day = dbResponse[i].day,
-        // newWorkoutObject.exercises = dbResponse[i].exercises
-          
-//             // INNER LOOP: Loops through all the exercises.
-//             //totalDuration += exercises[j].duration
-//             //INNER LOOP ENDS!!
-//         // OUTER LOOP, again:
-//             //Add total duration to newWorkoutObject.totalDuration = totalDuration, newWorkoutObject.day = dbResponse[i].day, newWorkoutObject.exercises = dbResponse[i].exercises
-//             // Push new object to newWorkoutArray
-//             // OUTER LOOP ends
-//     //     }
-
-//     //     console.log("Response", dbResponse);
-//         res.json(newWorkoutArray)
-//         // will later become:  res.json(newWorkoutArray) 
-//     })
-//     .catch(({ message }) => {
-//         console.log("error", message);
-//         res.send(400)
-//     });
-// });
-
 app.get("/api/workouts", (req, res) => {
-    db.Workout.find({})
+    db.Workout.find()
         .then(dbResponse => {
             let newWorkoutArray = [];
-            for (let i = 0; i < workout.length; i++) {
-                let newWorkoutObject;
-                let totalDuration = 0;
-                for (let j = 0; j < exercises.length; j++) {
-                    totalDuration += exercises[j].duration;
+            let totalDuration = 0;
+            for (let i = 0; i < dbResponse.length; i++) {
+                let newWorkoutObject = {};
+                for (let j = 0; j <  dbResponse[i].exercises.length; j++) {
+                    totalDuration += dbResponse[i].exercises[j].duration;
                 }
-                // newWorkoutObject = { day: workout[i].day, exercises: workout[i].exercises, totalDuration: totalDuration, _id: workout[i]._id }
                 newWorkoutObject.totalDuration = totalDuration,
                 newWorkoutObject.day = dbResponse[i].day,
                 newWorkoutObject.exercises = dbResponse[i].exercises;
+                console.log("Success!", newWorkoutObject);
                 newWorkoutArray.push(newWorkoutObject);
             }
             res.json(newWorkoutArray);
@@ -256,14 +94,6 @@ app.post("/api/workouts", (req, res) => {
         console.log(err)
     });
 });
-// app.get("/exercise", (req, res) => {
-//     db.find()
-//     .then(function(response){
-//         console.log(response)
-//     })
-//     res.sendFile("./public/exercise.html")
-// })
-
 
 // Start the server
 app.listen(PORT, () => {
